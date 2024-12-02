bf421bd12c3fc9223c45fb3f91b1a02a52a21b9d
erwin <erwin@metaregistrar.com>
2024-12-02 16:31:30 +0100

    Fixed configuration in example

--- example/dnszoneInfo.php
+++ example/dnszoneInfo.php
@@ -3,10 +3,9 @@
 use Metaregistrar\Api\Client\Connection;
 
 
-$hostname = '';
-$apiKey = '';
+$hostname = 'api-ote.metaregistrar.com';
+$apiKey = 'fillme';
 require_once(__DIR__ . "/../vendor/autoload.php");
-require_once(__DIR__ . "/config.php");
 
 
 $connection = new Connection($hostname, $apiKey);
